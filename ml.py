72af3a06f9f72e354f8481b6094ff33ac787e461
KnightsUCF <peterk@knights.ucf.edu>
2021-03-20 17:34:21 -0400

    Update ml.py

--- ml.py
+++ ml.py
@@ -1,5 +1,11 @@
+
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers.normalization import BatchNormalization
+from keras.optimizers import SGD
 
 
 
