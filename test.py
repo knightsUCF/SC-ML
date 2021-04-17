646c29752f3b33d56ce5451062a17d3f23c17283
KnightsUCF <peterk@knights.ucf.edu>
2021-04-16 21:57:46 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import preprocessing
+import ml
+
+preprocessing = preprocessing.Preprocessing()
+ml = ml.ML()
+
+
+
+# rerun preprocessing on data
+preprocessing.run() # here is when we can later pass in the different data file path
+
+# send preprocessed data to the ml model
+ml.load_data(preprocessing.get_train_set(), preprocessing.get_train_label(), preprocessing.get_test_set())
+
+# create model
+ml.create_superct_model()
+
+# train model
+ml.train_model()
